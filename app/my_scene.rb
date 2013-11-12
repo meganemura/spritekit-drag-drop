9c49e728ca785427e7035494a974f4eb053d85c4
meganemura <mura2megane@gmail.com>
2013-11-13 01:07:45 +0900

    Selecting Sprites based on Touches

--- app/my_scene.rb
+++ app/my_scene.rb
@@ -2,6 +2,8 @@ class MyScene < SKScene
 
   ANIMAL_NODE_NAME = 'movable'.freeze
 
+  attr_accessor :selected_node
+
   def initWithSize(size)
     super
 
@@ -24,17 +26,37 @@ class MyScene < SKScene
   end
 
   def touchesBegan(touches, withEvent: event)
-    touches.each do |touch|
-      location = touch.locationInNode(self)
-      sprite = SKSpriteNode.spriteNodeWithImageNamed("Spaceship")
-      sprite.position = location
-      action = SKAction.rotateByAngle(Math::PI, duration: 1)
-      sprite.runAction(SKAction.repeatActionForever(action))
-      self.addChild(sprite)
-    end
+    touch = touches.anyObject
+    position_in_scene = touch.locationInNode(self)
+    self.select_node_for_touch(position_in_scene)
   end
 
   def update(current_time)
     # Called before each frame is rendered
+  end
+
+  def select_node_for_touch(touch_location)
+    touched_node = self.nodeAtPoint(touch_location)
+    unless @selected_node == touched_node
+      if @selected_node
+        @selected_node.removeAllActions
+        @selected_node.runAction(SKAction.rotateToAngle(0.0, duration: 0.1))
+      end
+
+      @selected_node = touched_node
+
+      if touched_node.name == ANIMAL_NODE_NAME
+        sequence = SKAction.sequence([
+          SKAction.rotateByAngle(degrees_to_radians(-4.0), duration: 0.1),
+          SKAction.rotateByAngle(0.0, duration: 0.1),
+          SKAction.rotateByAngle(degrees_to_radians(4.0), duration: 0.1),
+        ])
+        @selected_node.runAction(SKAction.repeatActionForever(sequence))
+      end
+    end
+  end
+
+  def degrees_to_radians(degrees)
+    radians = degrees * Math::PI / 180
   end
 end
